0569059ec4a0f3c4890b3a48dab3df91cd516e89
Harris Yun <harrispooted2@gmail.com>
2017-04-15 20:17:19 -0700

    set up gravity changing

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -2,14 +2,33 @@
 using System.Collections;
 
 public class player : MonoBehaviour {
+	private bool isGrounded = false;
 
 	// Use this for initialization
-	void Start () {
-	
+	void OnCollisionEnter(Collision collide) {
+		if (collide.gameObject.tag == "Ground") {
+			isGrounded = true;
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if(isGrounded) {
+			if(Input.GetKey(KeyCode.W)) {
+				Physics.gravity = new Vector3(0, 9.81f, 0);
+			}
+
+			if(Input.GetKey(KeyCode.S)) {
+				Physics.gravity = new Vector3(0, -9.81f, 0);
+			}
+
+			if(Input.GetKey(KeyCode.A)) {
+				Physics.gravity = new Vector3(-9.81f, 0, 0);
+			}
+
+			if(Input.GetKey(KeyCode.D)) {
+				Physics.gravity = new Vector3(9.81f, 0, 0);
+			}
+		}
 	}
 }
