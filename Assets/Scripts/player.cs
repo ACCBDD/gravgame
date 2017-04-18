cf23a97f75d08c4d5004507b9ec27af2ca1a3883
Harris Yun <harrispooted2@gmail.com>
2017-04-17 19:14:21 -0700

    add initial ui stuff, bug fixing with changing gravity

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,33 +1,64 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+
 
 public class player : MonoBehaviour {
 	private bool isGrounded = false;
-
+	public Sprite[] arrowSprites = new Sprite[4];
+	public Image uiArrow;
+	public CanvasGroup canvasFlash;
+	private bool flash;
 	// Use this for initialization
-	void OnCollisionEnter(Collision collide) {
+	void OnCollisionStay(Collision collide) {
 		if (collide.gameObject.tag == "Ground") {
 			isGrounded = true;
 		}
 	}
 
+	void OnCollisionExit(Collision collide) {
+		if (collide.gameObject.tag == "Ground") {
+			isGrounded = false;
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
+		if (flash) {
+			canvasFlash.alpha = canvasFlash.alpha - Time.deltaTime * 3;
+						if (canvasFlash.alpha <= 0) {
+								canvasFlash.alpha = 0;
+								flash = false;
+						}
+		}
+
 		if(isGrounded) {
-			if(Input.GetKey(KeyCode.W)) {
+			if(Input.GetKeyDown(KeyCode.W) && uiArrow.sprite != arrowSprites[0]) {
 				Physics.gravity = new Vector3(0, 9.81f, 0);
+				uiArrow.sprite = arrowSprites[0];
+				flash = true;
+				canvasFlash.alpha = 1;
 			}
 
-			if(Input.GetKey(KeyCode.S)) {
+			if(Input.GetKeyDown(KeyCode.S) && uiArrow.sprite != arrowSprites[2]) {
 				Physics.gravity = new Vector3(0, -9.81f, 0);
+				uiArrow.sprite = arrowSprites[2];
+				flash = true;
+				canvasFlash.alpha = 1;
 			}
 
-			if(Input.GetKey(KeyCode.A)) {
+			if(Input.GetKeyDown(KeyCode.A) && uiArrow.sprite != arrowSprites[3]) {
 				Physics.gravity = new Vector3(-9.81f, 0, 0);
+				uiArrow.sprite = arrowSprites[3];
+				flash = true;
+				canvasFlash.alpha = 1;
 			}
 
-			if(Input.GetKey(KeyCode.D)) {
+			if(Input.GetKeyDown(KeyCode.D) && uiArrow.sprite != arrowSprites[1]) {
 				Physics.gravity = new Vector3(9.81f, 0, 0);
+				uiArrow.sprite = arrowSprites[1];
+				flash = true;
+				canvasFlash.alpha = 1;
 			}
 		}
 	}
